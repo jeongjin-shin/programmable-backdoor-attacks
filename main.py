8cf515218e0dc5eaba0ae44e5fa9a2d765f1f575
jeongjin-shin <80797980+jeongjin-shin@users.noreply.github.com>
2023-09-13 13:28:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import torchvision.transforms as transforms
 import torchvision
 
 import numpy as np
@@ -25,16 +26,15 @@ def get_data(args):
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
         
-        trainset = datasets.CIFAR10(root='./data', train=True,download=True, transform=transform_train)
-        testset = datasets.CIFAR10(root='./data', train=False,download=True, transform=transform_test)
+        trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform_train)
+        testset = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform_test)
 
-        train_loader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
-        test_loader = DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+        train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+        test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
         return train_loader,test_loader
-    if args.dataset == 'mnist':
-        
-    if args.dataset == 'gtsrb':
+    #if args.dataset == 'mnist':
+    #    if args.dataset == 'gtsrb':
 
 
 def get_model(args):
@@ -99,7 +99,7 @@ def train(
             
             test_correct, test_total = 0, 0
             train_correct, train_total = 0, 0
-            attack_success [0 for _ in range(len(target_label))]
+            attack_success = [0 for _ in range(len(target_label))]
 
             with torch.no_grad():
                 for inputs, labels in testloader:
